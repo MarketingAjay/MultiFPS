99815599599a1df687850459bd971310a55c6972
pwnageman2 <anishdhesikan@gmail.com>
2014-03-02 17:45:35 -0500

    add_shoot

---
+++ Assets/Scripts/ShootBall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootBall : MonoBehaviour {
+
+	public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (1)) {
+			Instantiate(ball, transform.position, Quaternion.identity);
+			ball.rigidbody.AddForce(Vector3.forward * 10);
+		}
+	}
+}
