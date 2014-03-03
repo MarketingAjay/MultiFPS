f5cc6c15248b3dc10467c29070d55a9b7667c090
pwnageman2 <anishdhesikan@gmail.com>
2014-03-02 19:10:01 -0500

    Modify Scene 1

--- Assets/Scripts/ShootBall.cs
+++ Assets/Scripts/ShootBall.cs
@@ -4,17 +4,30 @@ using System.Collections;
 public class ShootBall : MonoBehaviour {
 
 	public GameObject ball;
+	public Camera mainCamera;
+	public float ballSpeed = 5000f;
+
+	private Vector3 teleportPoint;
+	private GameObject instance;
+	private bool movingOn;
 
 	// Use this for initialization
 	void Start () {
-	
+		movingOn = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (1)) {
-			Instantiate(ball, transform.position, Quaternion.identity);
-			ball.rigidbody.AddForce(Vector3.forward * 10);
+		if (Input.GetMouseButtonDown (0)) {
+			instance = (GameObject) Instantiate(ball, transform.position + transform.forward, Quaternion.identity);
+			instance.rigidbody.AddForce(mainCamera.transform.forward * ballSpeed);
+			movingOn = true;
+		}
+
+		if (instance.GetComponent<TeleportCharacter>().firstColPoint != Vector3.zero && movingOn) {
+			teleportPoint = instance.GetComponent<TeleportCharacter>().firstColPoint;
+			transform.position = teleportPoint;
+			movingOn = false;
 		}
 	}
 }
---
+++ Assets/Scripts/TeleportCharacter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportCharacter : MonoBehaviour {
+
+	public bool teleportable;
+	public Vector3 firstColPoint = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		teleportable = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col) {
+		if (teleportable) {
+			firstColPoint = col.contacts[0].point + new Vector3 (0f, 5f, 0f);
+		}
+
+		teleportable = false;
+	}
+}
